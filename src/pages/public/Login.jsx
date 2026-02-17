8b7b0560436aec43e45b4f2c4fc9aaa85bc5efa1
badr554 <badrsalah012@gmail.com>
2026-02-17 15:11:22 +0200

    test login

--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -1,7 +1,9 @@
 import "../../styles/Login.css";
 
 const Login = () => {
-  return <div className="login">Login Page</div>;
+  return <div className="login">Login Page
+  <p> badr salah eldin 221001606</p>
+  </div>;
 };
 
 export default Login;
